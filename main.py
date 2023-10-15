0343c6567b9d70155d8274bd0729cb8c405ccffe
Bridgge <hochagro@gmial.com>
2023-10-15 18:29:20 +0200

    second commit

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import requests
+import json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+r = requests.get('https://httpbin.org/get')
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+r_json = json.loads(r.text)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(r_json)
